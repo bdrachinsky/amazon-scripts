613750ed24ba76a0db552f20b6d4ddb38852b7d8
bogdandrachinsky@gmail.com <bogdandrachinsky@gmail.com>
2018-01-24 20:03:53 +0200

    added custom policy

--- createCustomPolicy.js
+++ createCustomPolicy.js
@@ -10,34 +10,27 @@ let iam = new AWS.IAM({apiVersion: '2010-05-08'});
 let myManagedPolicy = {
     "Version": "2012-10-17",
     "Statement": [
-        {
-            "Effect": "Allow",
-            "Action": [
-                "autoscaling:Describe*",
-                "autoscaling:UpdateAutoScalingGroup",
-                "cloudformation:CreateStack",
-                "cloudformation:DeleteStack",
-                "cloudformation:DescribeStack*",
-                "cloudformation:UpdateStack",
-                "cloudwatch:GetMetricStatistics",
-                "ec2:Describe*",
-                "elasticloadbalancing:*",
-                "ecs:*",
-                "events:DescribeRule",
-                "events:DeleteRule",
-                "events:ListRuleNamesByTarget",
-                "events:ListTargetsByRule",
-                "events:PutRule",
-                "events:PutTargets",
-                "events:RemoveTargets",
-                "iam:ListInstanceProfiles",
-                "iam:ListRoles",
-                "iam:PassRole",
-            ],
-            "Resource": "*"
-        }
+      {
+        "Effect": "Allow",
+        "Action": [
+          "ecs:CreateCluster",
+          "ecs:DeregisterContainerInstance",
+          "ecs:DiscoverPollEndpoint",
+          "ecs:Poll",
+          "ecs:RegisterContainerInstance",
+          "ecs:StartTelemetrySession",
+          "ecs:Submit*",
+          "ecr:GetAuthorizationToken",
+          "ecr:BatchCheckLayerAvailability",
+          "ecr:GetDownloadUrlForLayer",
+          "ecr:BatchGetImage",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+        ],
+        "Resource": "*"
+      }
     ]
-};
+  };
 
 let params = {
     PolicyDocument: JSON.stringify(myManagedPolicy),
